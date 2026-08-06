f07e8fe44e9925ae9f2d3fcc686144aee4ae5364
Shruti Kumari <shrutikumari00030@gmail.com>
2026-08-06 17:15:43 +0530

    added book modules and features ,also edit and delete records

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
+const Book = require("../models/Book");
+
+router.get("/", async (req, res) => {
+
+    try {
+
+        const books = await Book.find();
+
+        res.render("books/index", { books: books });
+
+    } catch (error) {
+
+        res.send(error.message);
+
+    }
+
+});
+
 router.get("/add", (req, res) => {
     res.render("books/add");
 });
-
-const Book = require("../models/Book");
 
 router.post("/add", async (req, res) => {
 
@@ -25,6 +41,60 @@ router.post("/add", async (req, res) => {
 
     } catch (error) {
         res.send(error.message);
+    }
+
+});
+
+router.get("/edit/:id", async (req, res) => {
+
+    try {
+
+        const book = await Book.findById(req.params.id);
+
+        res.render("books/edit", { book: book });
+
+    } catch (error) {
+
+        res.send(error.message);
+
+    }
+
+});
+
+router.post("/edit/:id", async (req, res) => {
+
+    try {
+
+        await Book.findByIdAndUpdate(req.params.id, {
+            title: req.body.title,
+            author: req.body.author,
+            category: req.body.category,
+            isbn: req.body.isbn,
+            quantity: req.body.quantity
+        });
+
+        res.redirect("/books");
+
+    } catch (error) {
+
+        res.send(error.message);
+
+    }
+
+});
+
+router.post("/delete/:id", async (req, res) => {
+
+    try {
+
+        await Book.findByIdAndDelete(req.params.id);
+
+        res.redirect("/books");
+
+    } catch (error) {
+
+        res.send(error.message);
+
     }
 
 });
